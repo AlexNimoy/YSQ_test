942259af8414c340e9782194122f6c41d3606cdd
Alexander Polyakov <backstabe@gmail.com>
2024-10-13 23:11:01 +0400

    order blocks

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,7 +1,7 @@
-<script setup lang="ts">
-import Chart from '../components/test/Chart.vue'
-</script>
-
 <template>
   <Chart />
 </template>
+
+<script setup lang="ts">
+import Chart from '../components/test/Chart.vue'
+</script>
