b49c239d7e17dfc251780b103a66ca98ffb39933
Aleksandr Poliakov <backstabe@gmail.com>
2024-12-21 23:34:26 +0000

    Добавить футер с результатами и кнопкой для просмотра результата

--- src/pages/Questionnaire.vue
+++ src/pages/Questionnaire.vue
@@ -10,6 +10,20 @@
         />
       </section>
     </div>
+    <div class="footer">
+      <div class="results">
+          <router-link 
+          v-if="userAnswers.length === questions.length" 
+          to="/result" 
+          class="button"
+        >
+          📊 Просмотр результата
+        </router-link>
+        <div v-else>
+          Осталось ответить на {{ questions.length - userAnswers.length }} из {{ questions.length }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,3 +35,17 @@ import Answers from '../components/test/Answers.vue'
 
 const userAnswers = ref([])
 </script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+}
+.results {
+  background-color: var(--bg);
+  display: inline-block;
+  padding: 10px;
+}
+</style>
